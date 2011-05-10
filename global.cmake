e5703ea4aba95a83f077a6ead839b24a84679649
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-05-10 16:33:59 -0300

    * Added CMakeLists.txt (src/common) * global.cmake

---
+++ global.cmake
@@ -0,0 +1,77 @@
+# set( LIPI_ROOT $ENV{LIPI_ROOT}}
+set( LIPI_ROOT ${CMAKE_CURRENT_LIST_DIR} )
+
+set( LIPITK_BIN ${LIPI_ROOT}/bin )
+# set( LIPITK_DOC ${LIPI_ROOT}/doc )
+# set( LIPITK_LIB ${LIPI_ROOT}/lib )
+# set( LIPITK_PACK ${LIPI_ROOT}/package )
+# set( LIPITK_PROJ ${LIPI_ROOT}/projects )
+# set( LIPITK_SCRIPT ${LIPI_ROOT}/scripts )
+# set( LIPITK_UTILS ${LIPI_ROOT}/utils )
+set( LIPITK_SRC ${LIPI_ROOT}/src )
+# set( LIPITK_SRC_LIB ${LIPITK_SRC}/lib )
+# set( LIPITK_TMP ${LIPITK_PACK}/tmp )
+
+
+# set( LIPITK_DYNAMIC_LIBDIR ${LIPI_ROOT}/lib )
+# set( LIPITK_BIN ${LIPI_ROOT}/bin )
+# set( LIPITK_STATIC_LIBDIR ${LIPITK_SRC}/lib )
+set( LIPITK_SRC_INCLUDE ${LIPITK_SRC}/include )
+# set( LIPITK_LIPIENGINE ${LIPITK_SRC}/lipiengine )
+# set( LIPITK_RECO ${LIPITK_SRC}/reco )
+# set( LIPITK_RECO_UTIL ${LIPITK_RECO}/util )
+# set( LIPITK_TOOLS ${LIPITK_SRC}/tools )
+# set( LIPITK_SRC_APPS ${LIPITK_SRC}/apps )
+set( LIPITK_SRC_UTILS ${LIPITK_SRC}/util )
+set( LIPITK_SRC_UTILS_LIB ${LIPITK_SRC_UTILS}/lib )
+# set( LIPITK_LOGGER ${LIPITK_SRC_UTILS}/logger )
+# set( LIPITK_SRC_UTILS_TRAINHMM ${LIPITK_SRC_UTILS}/trainhmm )
+# set( LIPITK_SRC_UTILS_IMGWRITER ${LIPITK_SRC_UTILS}/imgwriter )
+# set( LIPITK_SRC_UTILS_RUN ${LIPITK_SRC_UTILS}/run )
+# set( LIPITK_SRC_UTILS_RUNSHAPEREC ${LIPITK_SRC_UTILS}/run/runshaperec )
+# set( LIPITK_SRC_UTILS_RUNWORDREC ${LIPITK_SRC_UTILS}/run/runwordrec )
+# set( LIPITK_SRC_UTILS_MDV ${LIPITK_SRC_UTILS}/mdv )
+# set( LIPITK_SRC_UTILS_FFW ${LIPITK_SRC_UTILS}/featurefilewriter )
+# set( LIPITK_COMMON ${LIPITK_SRC}/common )
+# set( LIPITK_SAMPLES ${LIPITK_SRC_APPS}/samples )
+
+#############################################
+#   SHAPEREC MACROS
+#############################################
+
+# set( LIPITK_SHAPEREC ${LIPITK_RECO}/shaperec )
+# set( LIPITK_SHAPEREC_COMMON ${LIPITK_SHAPEREC}/common )
+# set( LIPITK_SHAPEREC_PREPROC ${LIPITK_SHAPEREC}/preprocessing )
+# set( LIPITK_SHAPEREC_PCA ${LIPITK_SHAPEREC}/pca )
+# set( LIPITK_SHAPEREC_NN ${LIPITK_SHAPEREC}/nn )
+# set( LIPITK_SHAPEREC_UTILS ${LIPITK_SHAPEREC}/util )
+# set( LIPITK_SHAPEREC_TST ${LIPITK_SHAPEREC}/tst )
+# set( LIPITK_SHAPEREC_HMM ${LIPITK_SHAPEREC}/hmm )
+# set( LIPITK_SHAPEREC_ACTIVEDTW ${LIPITK_SHAPEREC}/activedtw )
+# set( LIPITK_SHAPEREC_NEURALNET ${LIPITK_SHAPEREC}/neuralnet )
+
+#############################################
+#   SHAPEREC FEATURE EXTRACTOR MACROS
+#############################################
+
+# set( LIPITK_SHAPEREC_FE ${LIPITK_SHAPEREC}/featureextractor )
+# set( LIPITK_SHAPEREC_FE_COMMON ${LIPITK_SHAPEREC_FE}/common )
+# set( LIPITK_SHAPEREC_FE_POINTFLOAT ${LIPITK_SHAPEREC_FE}/pointfloat )
+# set( LIPITK_SHAPEREC_FE_L7 ${LIPITK_SHAPEREC_FE}/l7 )
+# set( LIPITK_SHAPEREC_FE_NPEN ${LIPITK_SHAPEREC_FE}/npen )
+# set( LIPITK_SHAPEREC_FE_SS ${LIPITK_SHAPEREC_FE}/substroke )
+
+#############################################
+#   WORDREDC MACROS
+#############################################
+
+# set( LIPITK_WORDREC ${LIPITK_RECO}/wordrec )
+# set( LIPITK_WORDREC_COMMON ${LIPITK_WORDREC}/common )
+# set( LIPITK_WORDREC_BOXFLD ${LIPITK_WORDREC}/boxfld )
+
+
+#############################################
+#   SAMPLE MACROS
+#############################################
+# set( LIPITK_SAMPLES_SHAPERECTST ${LIPITK_SAMPLES}/shaperectst )
+# set( LIPITK_SAMPLES_WORDRECTST ${LIPITK_SAMPLES}/wordrectst )
