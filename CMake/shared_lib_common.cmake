38ab43dadad610bc1ad3a4011dee9cfef012d0a8
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-05-12 04:18:07 -0300

    Added many CMakeLists

---
+++ CMake/shared_lib_common.cmake
@@ -0,0 +1,17 @@
+include( ${CMAKE_CURRENT_LIST_DIR}/global.cmake )
+
+# Sources
+file( GLOB sources "${CMAKE_CURRENT_SOURCE_DIR}/*.cpp" )
+
+# Include
+include_directories( ${LIPITK_SRC_INCLUDE} ${LIPITK_SRC_UTILS_LIB} )
+
+# Shared
+add_library( ${PROJECT_NAME} SHARED ${sources} )
+
+# Install
+INSTALL(TARGETS ${PROJECT_NAME}
+  RUNTIME DESTINATION bin
+  LIBRARY DESTINATION lib
+  ARCHIVE DESTINATION lib/static
+)
